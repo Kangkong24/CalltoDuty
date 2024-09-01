efafa9781516def192cab867b4b80d220f868187
Josh <joshortiz820@gmail.com>
2024-09-01 11:24:29 +0800

    Second Commit

--- app/src/main/java/com/example/calltoduty/MainActivity.kt
+++ app/src/main/java/com/example/calltoduty/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calltoduty
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        Log.i("tag","Hello")
+        Log.i("tag","World")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
