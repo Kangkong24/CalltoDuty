5a6e8770a6f363bf9ae3a7bb44851743516b7571
Indignus12 <junegerald123@gmail.com>
2024-10-02 23:37:51 -0700

    login for account

---
+++ app/src/main/java/com/example/calltoduty/LoginPage.kt
@@ -0,0 +1,17 @@
+package com.example.calltoduty
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+
+    }
+}
