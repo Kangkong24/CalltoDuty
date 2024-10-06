f2c009a3975170be1ea002c2513dc78a9d11a655
Indignus12 <junegerald123@gmail.com>
2024-10-06 02:16:02 -0700

    ipabukas q na mona yung kolang d2 sa option page oks

---
+++ app/src/main/java/com/example/calltoduty/OptionPage.kt
@@ -0,0 +1,20 @@
+package com.example.calltoduty
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OptionPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_option_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
