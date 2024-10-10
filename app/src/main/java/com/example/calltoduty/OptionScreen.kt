f0416f057c4bba4732f23bdea3ba2f5a316f3455
Indignus12 <junegerald123@gmail.com>
2024-10-09 21:08:53 -0700

    update lang hahahaha

--- app/src/main/java/com/example/calltoduty/OptionScreen.kt
+++ app/src/main/java/com/example/calltoduty/OptionScreen.kt
@@ -2,40 +2,30 @@ package com.example.calltoduty
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class OptionPage : AppCompatActivity() {
+class OptionScreen : AppCompatActivity() {
 
-    private lateinit var closeButton: ImageView
-    private lateinit var musicButton: ImageView
-    private lateinit var musicSwitch: Switch
-    private lateinit var soundButton: ImageView
-    private lateinit var soundSwitch: Switch
-    private lateinit var cnButton: ImageView
-    private lateinit var deleteButton: ImageView
+    private lateinit var musicSwitch : Switch
+    private lateinit var soundSwitch : Switch
+    private lateinit var cnButton : Button
+    private lateinit var deleteButton : Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_option_screen)
 
-        closeButton = findViewById(R.id.closeButton)
-        musicButton = findViewById(R.id.musicButton)
         musicSwitch = findViewById(R.id.musicSwitch)
-        soundButton = findViewById(R.id.soundButton)
         soundSwitch = findViewById(R.id.soundSwitch)
         cnButton = findViewById(R.id.cnButton)
         deleteButton = findViewById(R.id.deleteButton)
-
-        closeButton.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
 
         cnButton.setOnClickListener{
             val intent = Intent(this, ChangeNIcknamePage::class.java)
